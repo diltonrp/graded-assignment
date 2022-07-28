148ab0aaf8040d5035efdbd21153a3d46f79c2d6
User <User@10.174.122.254>
2022-07-28 09:52:32 +0800

    Robert: correct spelling of teacher variable in C206_CaseStudyTest

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -10,9 +10,9 @@ public class C206_CaseStudyTest {
 	private Student sd1;
 	private Student sd2;
 	private Student sd3;
-	private Teacher tc1;
-	private Teacher tc2;
-	private Teacher tc3;
+	private teacher tc1;
+	private teacher tc2;
+	private teacher tc3;
 	private CCA cc1;
 	private CCA cc2;
 	private CCA cc3;
@@ -20,7 +20,7 @@ public class C206_CaseStudyTest {
 	private CCA cc5;
 	
 	private ArrayList<Student> studentList;
-	private ArrayList<Teacher> teacherList;
+	private ArrayList<teacher> teacherList;
 	private ArrayList<CCA> ccaList;
 	
 
@@ -29,9 +29,9 @@ public class C206_CaseStudyTest {
 		sd1 = new Student(21000000, "T0123456A", "Mark", "mark@gmail.com", 88888888, "DIT", "W67N", "");
 		sd2 = new Student(21000001, "T0184637B", "Jack", "jack@gmail.com", 88888887, "DIT", "W67N", "");
 		sd3 = new Student(21000002, "T0128475C", "Mary", "mary@gmail.com", 88888886, "DIT", "W67N", "");
-		tc1 = new Teacher(123, "Dilton", "Republ!c01", "dilton@gmail.com", 99999999);
-		tc2 = new Teacher(124, "Yong Jian", "Republ!c01", "yongjian@gmail.com", 99999998);
-		tc3 = new Teacher(125, "Robert", "Republ!c01", "lela@gmail.com", 99999997);
+		tc1 = new teacher(123, "Dilton", "Republ!c01", "dilton@gmail.com", 99999999);
+		tc2 = new teacher(124, "Yong Jian", "Republ!c01", "yongjian@gmail.com", 99999998);
+		tc3 = new teacher(125, "Robert", "Republ!c01", "lela@gmail.com", 99999997);
 		cc1 = new CCA(1, "Badminton", "Able to play badminton and have fun!", "15:00", "Monday", "Sports");
 		cc2 = new CCA(2, "Red Cross", "Save people when they are in need!", "16:00", "Friday", "Uniform Groups");
 		cc3 = new CCA(3, "Chorale", "Sing on stage!", "16:00", "Monday", "Performing Arts");
